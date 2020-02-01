19665fcc06c223cf8792331e7dbfb095340f362e
Luiz Fernando de Lira <luizflira@gmail.com>
2020-02-01 01:06:23 -0300

    first commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    
+    [Range(0.1f, 20f)]
+    public float movementSpeed;
+
+    //Local References of player properties
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    void Awake(){
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+    
+    void Update(){
+        if (Time.timeScale > 0 && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * movementSpeed;
+        else
+            rb.velocity = Vector2.zero;
+        Debug.Log(Input.GetAxis("Horizontal"));
+    }
+    
+}
---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour{
+    [Header("Itens in inventory")]
+    [SerializeField] private ItemResource _resource;
+    [SerializeField] private ItemSeed _seed;
+
+    [Header("Inventory Selector")] [SerializeField]
+    private int _selectedSlot;
+}
